a64b77cf93c3ba0618ef627f952f8a32cc580b51
murat-akdag <akdag_136@hotmail.com>
2021-11-10 22:11:27 +0300

    Initial commit

---
+++ Seri_iletisim/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Seri_iletisim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGonderilecek = new System.Windows.Forms.TextBox();
+            this.txtAlinacak = new System.Windows.Forms.TextBox();
+            this.btnYolla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtGonderilecek
+            // 
+            this.txtGonderilecek.Location = new System.Drawing.Point(25, 26);
+            this.txtGonderilecek.Multiline = true;
+            this.txtGonderilecek.Name = "txtGonderilecek";
+            this.txtGonderilecek.Size = new System.Drawing.Size(200, 173);
+            this.txtGonderilecek.TabIndex = 0;
+            this.txtGonderilecek.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtGonderilecek_KeyPress);
+            // 
+            // txtAlinacak
+            // 
+            this.txtAlinacak.Location = new System.Drawing.Point(295, 26);
+            this.txtAlinacak.Multiline = true;
+            this.txtAlinacak.Name = "txtAlinacak";
+            this.txtAlinacak.Size = new System.Drawing.Size(200, 173);
+            this.txtAlinacak.TabIndex = 1;
+            // 
+            // btnYolla
+            // 
+            this.btnYolla.Location = new System.Drawing.Point(150, 228);
+            this.btnYolla.Name = "btnYolla";
+            this.btnYolla.Size = new System.Drawing.Size(75, 23);
+            this.btnYolla.TabIndex = 2;
+            this.btnYolla.Text = "Yolla";
+            this.btnYolla.UseVisualStyleBackColor = true;
+            this.btnYolla.Click += new System.EventHandler(this.btnYolla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 353);
+            this.Controls.Add(this.btnYolla);
+            this.Controls.Add(this.txtAlinacak);
+            this.Controls.Add(this.txtGonderilecek);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGonderilecek;
+        private System.Windows.Forms.TextBox txtAlinacak;
+        private System.Windows.Forms.Button btnYolla;
+    }
+}
+
---
+++ Seri_iletisim/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seri_iletisim
+{
+    public partial class Form1 : Form
+    {
+        static SerialPort _serialPort;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            _serialPort = new SerialPort("COM5", 115200, Parity.None, 8, StopBits.One);
+            if (!_serialPort.IsOpen)
+            {
+                _serialPort.Open();
+                //tbRX.Text = "port opened :) ";
+                _serialPort.DiscardInBuffer();
+            }
+            
+           // else
+                //tbRX.Text = "port busy :( ";
+
+            _serialPort.DataReceived += _serialPort_DataReceived;
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        private string rxString;
+
+        private void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int bytes = _serialPort.BytesToRead;
+            byte[] buffer = new byte[bytes];
+            _serialPort.Read(buffer, 0, bytes);
+            rxString = System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+            this.Invoke(new EventHandler(displayText));
+            
+           
+        }
+
+        private void displayText(object o, EventArgs e)
+        {
+            txtAlinacak.AppendText(rxString);
+        }
+
+
+        private void txtGonderilecek_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (_serialPort.IsOpen)
+            {
+                Byte[] ch = new Byte[1];
+                ch[0] = (byte)e.KeyChar;
+                _serialPort.Write(ch, 0, 1);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _serialPort.Close();
+        }
+
+        private void btnYolla_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write(txtGonderilecek.Text);
+        }
+    }
+
+
+
+}
